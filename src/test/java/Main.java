d0e3a15621617cbdbb16855fa018eabbb9803b80
RhubarbDev <contact@rhubarbdev.net>
2024-07-23 20:35:49 +0100

    Fixed some things.

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -8,6 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         Path downloadPath = PatcherUtil.DownloadChromeDriver();
-
+        System.out.println(downloadPath);
     }
 }
