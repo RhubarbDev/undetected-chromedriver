c45d7827103f9b42094b872bfd686fc2332b859b
Rhubarb <r2@rhubarbdev.net>
2024-07-26 11:32:56 +0100

    Better user-agent generation.

--- src/main/java/Utils/UserAgentUtil.java
+++ src/main/java/Utils/UserAgentUtil.java
@@ -2,8 +2,29 @@ package Utils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import LooseVersion.LooseVersion;
 
 public final class UserAgentUtil {
+    private static final String[] versions = {
+            "Windows NT 10.0; Win64; x64",
+            "Macintosh; Intel Mac OS X 10_15_7",
+            "X11; Linux x86_64"
+    };
+
+    private static final String userAgent = "Mozilla/5.0 (%) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/% Mobile Safari/537.36";
+
+    public static String genUserAgent() {
+        String versionName = versions[PatcherUtil.determineOS().ordinal() % versions.length];
+        LooseVersion versionNumber = PatcherUtil.getInstalledChromeVersion();
+        String[] parts = versionNumber.toString().split("\\.");
+
+        if (parts.length > 0 && Integer.parseInt(parts[0]) >= 101) {
+            versionNumber = new LooseVersion(parts[0] + ".0".repeat(parts.length - 1));
+        }
+
+        return userAgent.replaceFirst("%", versionName).replaceFirst("%", versionNumber.toString());
+    }
+
     public static String fixUserAgent(String userAgent) {
         try {
             userAgent = userAgent.replaceFirst("Headless", "");
